a76262237f7e48a125ade4d825272360eab6081d
vlad gobr <vladgobr@yahoo.com>
2022-12-19 13:08:32 +0300

    test 6

--- src/main/java/project_utils/AreNumbersEqual.java
+++ src/main/java/project_utils/AreNumbersEqual.java
@@ -1,2 +1,31 @@
-package project_utils;public class AreNumbersEqual {
+package project_utils;
+
+public class AreNumbersEqual {
+
+//    Напишите алгоритм AreNumbersEqual, который принимает на вход 2 любых int числа, и возвращает
+//       0, если числа равны
+//      -1, если первое число меньше второго
+//       1, если первое число больше второго
+
+//    Test Data:
+//         - 89, 89
+//    Expected result: 0
+//            -89, 89
+//    Expected result: -1
+//            89, -89
+//    Expected result: 1
+
+
+    public int areNumbersEqual (int numvl1, int numvl2) {
+        if (numvl1 == numvl2) {
+
+            return 0;
+        } else if (numvl1 < numvl2) {
+
+            return -1;
+        } else  {
+            return 1;
+        }
+    }
+
 }
--- src/test/java/AreNumbersEqualTest.java
+++ src/test/java/AreNumbersEqualTest.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class AreNumbersEqualTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import project_utils.AreNumbersEqual;
+
+public class AreNumbersEqualTest {
+
+    @Test
+    public void testHappyAreNumbersEqualZero() {
+        int numvl1 = 89;
+        int numvl2 = 89;
+        int expectedResult = 0;
+
+        int actualResult = new AreNumbersEqual().areNumbersEqual(numvl1,numvl2);
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void testHappyAreNumbersEqualMinus() {
+        int numvl1 = -89;
+        int numvl2 = 89;
+        int expectedResult = -1;
+
+        int actualResult = new AreNumbersEqual().areNumbersEqual(numvl1,numvl2);
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+
+    @Test
+    public void testHappyAreNumbersEqualPlus() {
+        int numvl1 = 89;
+        int numvl2 = -89;
+        int expectedResult = 1;
+
+        int actualResult = new AreNumbersEqual().areNumbersEqual(numvl1,numvl2);
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
 }
